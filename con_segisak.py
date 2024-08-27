4901b1f1590b9a162192ed6ece11a533a8b5768c
_moliNasil <41929823+nahuelmol@users.noreply.github.com>
2024-08-27 12:12:27 -0300

    Create con_segisak.py

---
+++ con_segisak.py
@@ -0,0 +1,40 @@
+import segysak
+import pandas as pd
+import matplotlib.pyplot as plt
+import pathlib
+import xarray as xr
+
+from segysak.segy import segy_header_scan
+from segysak.segy import segy_loader
+from IPython.display import display
+
+long_data = '/content/drive/MyDrive/samples/3D_gathers_pstm_nmo.sgy'
+V3D_path = pathlib.Path(long_data)
+
+scan = segy_header_scan(long_data)
+with pd.option_context("display.max_rows", 91):
+    display(scan) #con esto se pintan los metadatos
+#en este caso la salida de metadatos tira
+#CDP_X=181
+#CDP_Y=185
+#inline_3d=189
+#crossline_3d=193
+#shotpoint=197
+#entre otros
+#esos datos se ingresan a continuación
+
+V3D = xr.open_dataset(
+    V3D_path,
+    dim_byte_fields={"ILINE_3D":189, "CROSSLINE_3D": 193, "ShotPoint":197},
+    extra_byte_fields={"CDP_X":181, "CDP_Y":185 },
+)
+V3D #para ver el iline y xline que se utilizam en la grafica
+#en este caso es ILINE_3D=1290 y CROSSLINE_3D=1150
+
+fig, ax1 = plt.subplots(ncols=1, figsize=(15, 8))
+V3D.data.transpose("ILINE_3D", "CROSSLINE_3D", "ShotPoint", "samples", transpose_coords=True).sel(
+    ILINE_3D=1290, CROSSLINE_3D=1150
+).plot(yincrease=False, cmap="seismic_r")
+plt.grid("grey")
+plt.ylabel("TWT")
+plt.xlabel("XLINE")
